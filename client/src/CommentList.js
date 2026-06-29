7bad111f730484421cb364b10b0a2afe35d5e63d
shaheeramjad <shaheeransari450@gmail.com>
2026-06-29 16:14:39 +0500

    Rendered on Status Now

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -5,7 +5,16 @@ export default ({comments}) => {
 
 
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>
+        let content;
+        if (comment.status === 'approved') {
+            content = comment.content;
+        } else if (comment.status === 'rejected') {
+            content = 'This comment has been rejected';
+        } else if (comment.status === 'pending') {
+            content = 'This comment is awaiting moderation';
+        }
+
+        return <li key={comment.id}>{content}</li>
     });
     return (
         <ul>
--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,12 @@ const axios = require ('axios');
 const app = express ();
 app.use (bodyParser.json ());
 
+const events = [];
+
 app.post ('/events', async (req, res) => {
   const event = req.body;
+
+  events.push (event);
 
   const targets = [
     'http://localhost:4000/events',
@@ -25,6 +29,10 @@ app.post ('/events', async (req, res) => {
 
   res.send ({ status: 'OK' });
 }); 
+
+app.get ('/events', (req, res) => {
+  res.send (events);
+});
 
 
 app.listen (4005, () => {
